65b1ae0355110e306d29d554a814f9f737a8553b
dune <dune.amr-tardif@polytechnique.edu>
2025-09-05 12:57:04 +0200

    Init

--- main.ts
+++ main.ts
@@ -1,134 +1,428 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Modal, Notice, Plugin, PluginSettingTab, Setting } from "obsidian";
+import * as wanakana from "wanakana";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface WaniKaniPluginSettings {
+  apiToken: string;
+  radicalColor: string;
+  kanjiColor: string;
+  vocabularyColor: string;
+  maxReviews: number;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: WaniKaniPluginSettings = {
+  apiToken: "",
+  radicalColor: "#1173cf",
+  kanjiColor: "#e73c83",
+  vocabularyColor: "#7725d4",
+  maxReviews: 10,
+};
+
+if (!document.getElementById("confetti-script")) {
+  const script = document.createElement("script");
+  script.id = "confetti-script";
+  script.src = "https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js";
+  document.head.appendChild(script);
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
 
-	async onload() {
-		await this.loadSettings();
+export default class WaniKaniGatekeeperPlugin extends Plugin {
+  settings: WaniKaniPluginSettings;
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+  async onload() {
+    await this.loadSettings();
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+    this.app.workspace.onLayoutReady(async () => {
+      if (!this.settings.apiToken) {
+        new Notice("WaniKani API Token not set. Please configure in settings.");
+        return;
+      }
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+      try {
+        const reviews = await this.getReviews();
+        let reviews_dict: {[key: string]: [number, number]} = {};
+        reviews.forEach((rev) => {
+          reviews_dict[rev.id] = [0,0];
+        });
+        console.log("Current reviews:", reviews);
+        if (reviews.length != 0) {
+          new WaniKaniModal(this.app, this).open();
+        }
+      } catch (e) {
+        console.error("Error fetching WaniKani reviews:", e);
+        new Notice("Could not fetch WaniKani reviews.");
+      }
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+    });
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+    this.addSettingTab(new WaniKaniSettingTab(this.app, this));
+  }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+  onunload() {}
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
 
-	onunload() {
+  async getReviews(): Promise<any[]> {
+    const summaryRes = await fetch("https://api.wanikani.com/v2/summary", {
+      headers: {
+        "Wanikani-Revision": "20170710",
+        Authorization: `Bearer ${this.settings.apiToken}`,
+      },
+    });
+  
+    if (!summaryRes.ok) throw new Error(`Failed to fetch summary: ${summaryRes.status}`);
+  
+    const summaryData = await summaryRes.json();
+    const availableReviews = summaryData.data.reviews[0].subject_ids;
+  
+    if (availableReviews.length === 0) return [];
+  
+    const subjectsRes = await fetch(
+      `https://api.wanikani.com/v2/subjects?ids=${availableReviews.join(",")}`,
+      {
+        headers: {
+          "Wanikani-Revision": "20170710",
+          Authorization: `Bearer ${this.settings.apiToken}`,
+        },
+      }
+    );
+  
+    if (!subjectsRes.ok) throw new Error(`Failed to fetch subjects: ${subjectsRes.status}`);
+  
+    const subjectsData = await subjectsRes.json();
+    return subjectsData.data;
+  }
 
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+  async submitReview(subject_id: number ,incorrect_meaning_answers: number, incorrect_reading_answers: number) {
+    const payload = {
+      review: {
+        subject_id: subject_id,
+        incorrect_meaning_answers: incorrect_meaning_answers,
+        incorrect_reading_answers: incorrect_reading_answers,
+      }
+    };
+    
+  
+    const res = await fetch("https://api.wanikani.com/v2/reviews", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Wanikani-Revision": "20170710",
+        Authorization: `Bearer ${this.settings.apiToken}`,
+      },
+      body: JSON.stringify(payload),
+    });
+  
+    if (!res.ok) {
+      console.error("Failed to submit review:", await res.text());
+    } else {
+      console.log(`Submitted review for assignment ${subject_id}`);
+    }
+  }
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
+}
+function normalizeAnswer(text: string): string {
+  return text
+    .trim()
+    .toLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "");
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+class WaniKaniModal extends Modal {
+  plugin: WaniKaniGatekeeperPlugin;
+  checkInterval: number | null = null;
+  reviews: any[] = [];
+  currentIndex: number = 0;
+  reviews_dict: {[key: number]: any} = {};
+  allowEmergencyExit: boolean = false;
+  emergencyExitHandler: ((e: KeyboardEvent) => void) | null = null;
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+  constructor(app: App, plugin: WaniKaniGatekeeperPlugin) {
+    super(app);
+    this.plugin = plugin;
+  }
+  
+  renderNextReview() {
+    const { contentEl } = this;
+    contentEl.empty();
+  
+    if (this.currentIndex >= this.reviews.length) {
+      new Notice("All WaniKani reviews complete ✅");
+
+
+      const confetti = (window as any).confetti;
+      if (confetti) {
+        confetti({
+          particleCount: 200,
+          spread: 70,
+          origin: { y: 0.6 }
+        });
+      }
+  
+      const closeButton = this.modalEl.querySelector(".modal-close-button") as HTMLElement;
+      if (closeButton) closeButton.style.display = "block";
+  
+      setTimeout(() => this.close(), 1500);
+      return;
+    }
+  
+    const rev = this.reviews[this.currentIndex];
+  
+    const needsMeaning = Array.isArray(rev.data.meanings) && rev.data.meanings.length > 0;
+    const needsReading = Array.isArray(rev.data.readings) && rev.data.readings.length > 0;
+
+    let headerClass = "wanikani-header";
+    if (rev.object === "radical") {headerClass += " radical"; let obj = "radical";}
+    else if (rev.object === "kanji") {headerClass += " kanji"; let obj = "kanji";}
+    else if (rev.object === "vocabulary") {headerClass += " vocabulary"; let obj = "vocabulary";}
+    else if (rev.object === "kana_vocabulary") {headerClass += " radical"; let obj = "kana";};
+
+    contentEl.createDiv({ cls: headerClass}, (header) => {
+      header.createEl("h2", { text: `${rev.object.toUpperCase()}`, cls: "wanikani-object" });
+      header.createEl("div", { text: rev.data.characters || "", cls: "wanikani-character" });
+    });
+    
+    let meaningInput: HTMLInputElement | null = null;
+    let readingInput: HTMLInputElement | null = null;
+
+    contentEl.createDiv({ cls: "wanikani-section" }, (section) => {
+      if (needsMeaning) {
+        section.createEl("h1", { text: `Meaning` });
+        meaningInput = section.createEl("input", { 
+          type: "text", 
+          placeholder: "Your Response",
+          cls: "wanikani-input"
+        }) as HTMLInputElement;
+        setTimeout(() => meaningInput?.focus(), 0);
+      }
+    
+      if (needsReading) {
+        section.createEl("h1", { text: `Reading` });
+      
+        readingInput = section.createEl("input", { 
+          type: "text", 
+          placeholder: "Your Response",
+          cls: "wanikani-input"
+        }) as HTMLInputElement;
+        if (!needsMeaning) setTimeout(() => readingInput?.focus(), 0);
+        readingInput.addEventListener("input", (e) => {
+          const input = e.target as HTMLInputElement;
+          input.value = wanakana.toHiragana(input.value);
+        });
+      }
+    });
+    
+    const feedback = contentEl.createEl("p", { cls: "wanikani-feedback" });
+    const btn = contentEl.createEl("button", {cls: "wanikani-submit", text: "Submit" });
+    
+    const submitOnEnter = (inp: HTMLInputElement | null, btn: HTMLButtonElement) => {
+      if (!inp) return;
+      inp.addEventListener("keydown", (e: KeyboardEvent) => {
+        if (e.key === "Enter") {
+          e.preventDefault();
+          btn.click();
+        }
+      });
+    };
+    
+    submitOnEnter(meaningInput, btn);
+    submitOnEnter(readingInput, btn);
+    btn.onclick = async () => {
+      let meaningCorrect = true;
+      let readingCorrect = true;
+  
+      if (needsMeaning && meaningInput) {
+        const answer = normalizeAnswer(meaningInput.value);
+        const correctMeanings = [
+          ...rev.data.meanings.map((m: any) => normalizeAnswer(m.meaning)),
+          ...(rev.data.user_synonyms || []).map((s: string) => normalizeAnswer(s))
+        ];
+        meaningCorrect = correctMeanings.includes(answer);
+      }
+  
+      if (needsReading && readingInput) {
+        const answer = normalizeAnswer(readingInput.value);
+        const correctReadings = rev.data.readings
+          .filter((r: any) => r.primary)
+          .map((r: any) => normalizeAnswer(r.reading));
+        readingCorrect = correctReadings.includes(answer);
+      }
+
+      const isCorrect = meaningCorrect && readingCorrect;
+  
+      feedback.setText(isCorrect ? "✅ Correct!" : "❌ Incorrect!");
+      feedback.style.color = isCorrect ? "green" : "red";
+      if (!isCorrect) {
+        if (this.reviews_dict[rev.id]) {
+          this.reviews_dict[rev.id].incorrect_meaning_answers += meaningCorrect ? 0 : 1;
+          this.reviews_dict[rev.id].incorrect_reading_answers += readingCorrect ? 0 : 1;
+        } else {
+          this.reviews_dict[rev.id] = {
+            subject_id: rev.id,
+            incorrect_meaning_answers: meaningCorrect ? 0 : 1,
+            incorrect_reading_answers: readingCorrect ? 0 : 1
+          };
+        }
+        if (!readingCorrect) {
+        const correctReadings = rev.data.readings
+          .filter((r: any) => r.primary)
+          .map((r: any) => r.reading)
+          .join(", ");
+        feedback.setText(`❌ Incorrect! Correct readings: ${correctReadings}`);
+        }
+        if (!meaningCorrect) {
+        const correctMeanings = rev.data.meanings
+          .map((m: any) => m.meaning)
+          .join(", ");
+        feedback.setText(`❌ Incorrect! Correct meanings: ${correctMeanings}`);
+        }
+        this.currentIndex++;
+        setTimeout(() => {this.renderNextReview();}, 3000);
+        return;
+      } else {
+        let incorrect_meaning_answers = 0;
+        let incorrect_reading_answers = 0;
+        if (this.reviews_dict[rev.id]) {
+          incorrect_meaning_answers = this.reviews_dict[rev.id].incorrect_meaning_answers;
+          incorrect_reading_answers = this.reviews_dict[rev.id].incorrect_reading_answers;
+          delete this.reviews_dict[rev.id];
+        }
+        
+        await this.plugin.submitReview(rev.id, incorrect_meaning_answers, incorrect_reading_answers);
+      }  
+      this.currentIndex++;
+      setTimeout(() => this.renderNextReview(), 1000);
+    };
+    if (rev.data.characters) {
+      contentEl.createEl("p", { text: rev.data.characters, cls: "wanikani-character" });
+    }
+  }
+
+  async onOpen() {
+    const { contentEl, modalEl } = this;
+    contentEl.empty();
+    contentEl.createEl("h2", { text: "Complete your WaniKani Reviews" });
+    this.modalEl.addEventListener("mousedown", (e) => {
+      if (!this.contentEl.contains(e.target as Node)) {
+        e.stopPropagation();
+        e.preventDefault();
+      }
+    }, true);
+
+    (this.modalEl.parentElement as HTMLElement).style.pointerEvents = "none";
+    this.modalEl.style.pointerEvents = "auto"; 
+    this.reviews = await this.plugin.getReviews();
+    console.log("Fetched reviews:", this.reviews);
+    this.currentIndex = 0;
+  
+    const closeButton = modalEl.querySelector(".modal-close-button") as HTMLElement;
+    if (closeButton) closeButton.style.display = "none";
+
+    this.renderNextReview();
+    this.allowEmergencyExit = false;
+
+    this.emergencyExitHandler = (e: KeyboardEvent) => {
+      if (e.ctrlKey && e.altKey && e.key.toLowerCase() === "w") {
+        this.allowEmergencyExit = true;
+        new Notice("Emergency exit activated");
+        this.close();
+      }
+};
+document.addEventListener("keydown", this.emergencyExitHandler);
+  }
+  onClose() {
+    if (!this.allowEmergencyExit) {
+      new Notice(`You must complete at least ${this.plugin.settings.maxReviews} reviews!`);
+      return;
+    }
+    this.contentEl.empty();
+  }
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class WaniKaniSettingTab extends PluginSettingTab {
+  plugin: WaniKaniGatekeeperPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+  constructor(app: App, plugin: WaniKaniGatekeeperPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-	display(): void {
-		const {containerEl} = this;
+  display(): void {
+    const { containerEl } = this;
+    containerEl.empty();
 
-		containerEl.empty();
+    containerEl.createEl("h2", { text: "WaniKani Gatekeeper Settings" });
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    new Setting(containerEl)
+      .setName("Max Reviews")
+      .setDesc("Limit the number of reviews necessary to display gatekeeper exit")
+      .addText((text) =>
+        text
+          .setValue(this.plugin.settings.maxReviews.toString())
+          .onChange(async (value) => {
+            this.plugin.settings.maxReviews = Number(value) || 10;
+            await this.plugin.saveSettings();
+          })
+      );
+
+    new Setting(containerEl)
+      .setName("API Token")
+      .setDesc("Enter your WaniKani API token")
+      .addText((text) =>
+        text
+          .setValue(this.plugin.settings.apiToken)
+          .onChange(async (value) => {
+            this.plugin.settings.apiToken = value.trim();
+            await this.plugin.saveSettings();
+          })
+      );
+
+    new Setting(containerEl)
+      .setName("Radical Color")
+      .setDesc("Color for radical headers")
+      .addText((text) =>
+        text
+          .setValue(this.plugin.settings.radicalColor)
+          .onChange(async (value) => {
+            this.plugin.settings.radicalColor = value.trim() || "#1173cf";
+            await this.plugin.saveSettings();
+          })
+      );
+
+    new Setting(containerEl)
+      .setName("Kanji Color")
+      .setDesc("Color for kanji headers")
+      .addText((text) =>
+        text
+          .setValue(this.plugin.settings.kanjiColor)
+          .onChange(async (value) => {
+            this.plugin.settings.kanjiColor = value.trim() || "#e73c83";
+            await this.plugin.saveSettings();
+          })
+      );
+
+    new Setting(containerEl)
+      .setName("Vocabulary Color")
+      .setDesc("Color for vocabulary headers")
+      .addText((text) =>
+        text
+          .setValue(this.plugin.settings.vocabularyColor)
+          .onChange(async (value) => {
+            this.plugin.settings.vocabularyColor = value.trim() || "#7725d4";
+            await this.plugin.saveSettings();
+          })
+      );
+
+    containerEl.createEl("hr");
+    containerEl.createEl("h3", { text: "Usage Instructions" });
+    containerEl.createEl("p", { text: "1. Set your WaniKani API token above." });
+    containerEl.createEl("p", { text: "2. When you have reviews available, the gatekeeper popup will appear." });
+    containerEl.createEl("p", { text: "3. Complete at least the specified number of reviews to close the popup." });
+    containerEl.createEl("p", { text: "4. If you need to access your vault urgently, press Ctrl+Alt+W or Esc to close the modal." });
+  }
 }
